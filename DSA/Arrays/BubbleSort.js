ff0c122d6b56c48a9b77cbfc450e5325972f0d06
sohamsshah <sohamshah456@gmail.com>
2021-10-02 14:42:41 +0530

    add bubble-sort

---
+++ DSA/Arrays/BubbleSort.js
@@ -0,0 +1,22 @@
+// Bubble sort Implementation using Javascript
+  
+ function bblSort(arr){
+     
+ for(var i = 0; i < arr.length; i++){
+     
+   for(var j = 0; j < ( arr.length - i -1 ); j++){
+       
+     if(arr[j] > arr[j+1]){
+         
+       var temp = arr[j]
+       arr[j] = arr[j + 1]
+       arr[j+1] = temp
+     }
+   }
+ }
+ console.log(arr);
+}
+  
+var arr = [234, 43, 55, 63,  5, 6, 235, 547];
+  
+bblSort(arr);
