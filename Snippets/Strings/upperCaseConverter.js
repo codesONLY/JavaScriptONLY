a94b745733040f003e941be9ea8b27e976748d75
Meghana <meghana.meghanask.sk@gmail.com>
2021-10-20 09:51:02 +0530

    js reference book added  (#65)
    * added simple snippets for includes, indexOf, toString, isArray methods
    
    * #59 added simple code snippets for search, trim and repeat string methods
    
    * added snippets related to toUpperCase and toLowerCase methods
    
    * docs: Added a refernce bookon Javascript

---
+++ Snippets/Strings/upperCaseConverter.js
@@ -0,0 +1,7 @@
+// toUpperCase() method changes the string to upper letters.
+let favLanguage = "JavasCript";
+console.log(favLanguage.toUpperCase()); // JAVASCRIPT
+let firstName = "ECMA";
+console.log(firstName.toUpperCase()); // ECMA
+let oneMoreLangugae = "Python";
+console.log(oneMoreLangugae.toUpperCase()); // PYTHON
