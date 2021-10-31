42d3f8dbbcf0314690d41a8bd4330e0d94552332
mkpsdeat <93002284+mkpsdeat@users.noreply.github.com>
2021-10-31 22:36:54 +0530

    Create MiddleOfTheLinkedList.js

---
+++ SDE_Sheet/MiddleOfTheLinkedList.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
