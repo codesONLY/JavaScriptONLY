8218d90f7cdc4b719c5dbb1f8fac0e89dec82eca
Soham Shah <sohamshah456@gmail.com>
2022-05-30 10:25:20 +0530

    feat: add calculator chaining code

--- Snippets/Objects/calculator.js
+++ Snippets/Objects/calculator.js
@@ -7,10 +7,12 @@ class Calculator {
   sum() {
     console.log(this.a);
     console.log(this.a + this.b);
+    return this;
   }
 
   mul() {
     console.log(this.a * this.b);
+    return this;
   }
 }
 
@@ -35,3 +37,54 @@ function Calc(a, b) {
 const calc = new Calc(5, 2);
 console.log(calc.sum());
 console.log(calc.mul());
+
+// approach 2: with chaining
+
+function Calculator(initialValue) {
+  this.total = initialValue;
+  this.sum = function (a) {
+    this.total += a;
+    return this;
+  };
+  this.sub = function (a) {
+    this.total -= a;
+    return this;
+  };
+  this.misc = function (cb) {
+    this.total = cb(this.total);
+    return this;
+  };
+  this.val = function () {
+    return this.total;
+  };
+}
+
+const cal = new Calculator(2);
+console.print(
+  cal
+    .sum(2)
+    .misc((val) => val + 100)
+    .val()
+);
+
+// approach 3: using functions
+
+function cal(initialValue) {
+  let total = initialValue;
+
+  function add(a) {
+    total += a;
+    return cal(total);
+  }
+
+  function val() {
+    return total;
+  }
+
+  return {
+    add,
+    val,
+  };
+}
+
+console.print(cal(2).add(2).add(2).val());
