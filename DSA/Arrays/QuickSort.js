304d7dd95745279942a621a672c41011529d56dd
kevin shah <kevinstech0@gmail.com>
2023-10-28 13:32:25 +0530

    created quick sort algo

---
+++ DSA/Arrays/QuickSort.js
@@ -0,0 +1,19 @@
+const quickSort = (arr) => {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    let pivot = arr[0];
+    let leftArr = [];
+    let rightArr = [];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        leftArr.push(arr[i]);
+      } else {
+        rightArr.push(arr[i]);
+      }
+    }
+  
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  };
