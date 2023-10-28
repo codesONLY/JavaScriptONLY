c53c1514bbbcfdd47cf6731380bbab18811c63d6
Meet Javiya <107299231+meet-javiya0@users.noreply.github.com>
2023-10-28 15:41:27 +0530

    Update twoSum.js

--- SDE_Sheet/twoSum.js
+++ SDE_Sheet/twoSum.js
@@ -5,19 +5,18 @@
  */
 /* A brute-force approach. Traverse the nums array via two pointers i and j and return
  indexes stored in an array if the total matches the target*/
- var twoSum = function(nums, target) {
-    let targetResult =[];
-     
-     for(let i =0; i< nums.length; i++){
-         for(let j = i+1; j< nums.length; j++){
-             
-             if( nums[i]+ nums[j] === target){
-                 
-                 targetResult.push(i);
-                 targetResult.push(j);
-             }
-         }
-     }
-     
-     return targetResult;
- };
+var twoSum = function (nums, target) {
+  let targetResult = [];
+  // traverse the array via two pointers
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      // check if the sum of two numbers is equal to the target
+      if (nums[i] + nums[j] === target) {
+        targetResult.push(i);
+        targetResult.push(j);
+      }
+    }
+  }
+  // return the array with indexes
+  return targetResult;
+};
