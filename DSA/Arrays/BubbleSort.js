d0c5ee350fa00233eec636a28bc5892568188166
Dhrumil Rana <dhrumilrana@gmail.com>
2023-10-28 13:30:27 +0530

    this is basic massage

--- DSA/Arrays/BubbleSort.js
+++ DSA/Arrays/BubbleSort.js
@@ -1,20 +1,20 @@
 // Bubble sort Implementation using Javascript
   
- function bblSort(arr){
+function bblSort(arr){
    
- // loop through array
- for(var i = 0; i < arr.length; i++){
+  // loop through array
+for(var i = 0; i < arr.length; i++){
      
-   for(var j = 0; j < ( arr.length - i -1 ); j++){
+    for(var j = 0; j < ( arr.length - i -1 ); j++){
        
-     if(arr[j] > arr[j+1]){
+      if(arr[j] > arr[j+1]){
          
-       var temp = arr[j]
-       arr[j] = arr[j + 1]
-       arr[j+1] = temp
-     }
-   }
- }
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j+1] = temp
+      }
+    }
+  }
  console.log(arr);
 }
   
